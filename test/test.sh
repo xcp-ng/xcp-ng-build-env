ba4edca688028c7a87f727dcdbd7b7d5e899a0c9
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2025-09-05 23:47:22 +0200

    Fix linter warnings
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- test/test.sh
+++ test/test.sh
@@ -3,18 +3,21 @@
 set -eux
 
 TARGET_XCP_NG_VERSION="8.2"
+REPOS=xcp-emu-manager
+
+REPO_PATH=$(mktemp --directory --tmpdir xcp-buildenv-test.XXXXXX)
+trap "rm -rf '$REPO_PATH'" EXIT
+
+# clone first, to be sure that the token won't have expired
+for REPO in ${REPOS}; do
+    git clone --branch "$TARGET_XCP_NG_VERSION" https://github.com/xcp-ng-rpms/"$REPO" "$REPO_PATH/$REPO"
+done
 
 xcp-ng-dev-env-create "$TARGET_XCP_NG_VERSION"
-
-REPOS=xcp-emu-manager
 
 CONTAINER_NAME=${CONTAINER_NAME:-build-env}
 
 for REPO in ${REPOS}; do
-    REPO_PATH=$(mktemp --directory --tmpdir xcp-buildenv-test.XXXXXX)
-    trap "rm -rf '$REPO_PATH'" EXIT
-    git clone --branch "$TARGET_XCP_NG_VERSION" https://github.com/xcp-ng-rpms/"$REPO" "$REPO_PATH"
-
-    xcp-ng-dev container build "$TARGET_XCP_NG_VERSION" "$REPO_PATH" \
+    xcp-ng-dev container build "$TARGET_XCP_NG_VERSION" "$REPO_PATH/$REPO" \
         --name "$CONTAINER_NAME"
 done
