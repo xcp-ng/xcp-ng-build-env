c125ad808af6e5d8df31bca07badf9a3a08dabc6
Yann Dirson <yann.dirson@vates.tech>
2025-09-09 13:57:07 +0200

    Add support for bootstrapping the release package
    To build release package, we need a pure-upstream buildenv (and
    incidently, an older xcp-ng-release currently becomes uninstallable
    every time upstream updates almalinux-release, breaking existing
    containers).
    
    We also have a need for a buildenv where xcp-ng-release has taken the
    place of upstream's almalinux-release.
    
    This is done by introducing "buildenv variants", the pre-existing one
    being dubbed "build", and the new ones "bootstrap" and "isarpm" (current
    name of the bitbake layer needing it).
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>
    Signed-off-by: Julian Vetter <julian.vetter@vates.tech>

--- src/xcp_ng_dev/cli.py
+++ src/xcp_ng_dev/cli.py
@@ -114,6 +114,10 @@ def add_common_args(parser):
     group.add_argument('-U', '--enable-upstream-repos', action='store_true', help='enable the upstream repositories')
     group.add_argument('--no-update', action='store_true',
                        help='do not run "yum update" on container start, use it as it was at build time')
+    group.add_argument('--bootstrap', action='store_true',
+                       help='use a bootstrap build-env, able to build xc-ng-release')
+    group.add_argument('--isarpm', action='store_true',
+                       help='(internal) use a build-env suitable for the ISARPM build system')
     group.add_argument('--no-network', action='store_true',
                        help='disable all networking support in the build environment')
 
@@ -385,8 +389,14 @@ def container(args):
     # Set the timezone of the container so it corresponds to the local machine
     docker_args += ["-e", f"TZ={get_timezone()}"]
 
+    tag = args.container_version
+    if args.bootstrap:
+        tag += "-bootstrap"
+    if args.isarpm:
+        tag += "-isarpm"
+
     # exec "docker run"
-    docker_args += [f"{CONTAINER_PREFIX}:{args.container_version}",
+    docker_args += [f"{CONTAINER_PREFIX}:{tag}",
                     "/usr/local/bin/init-container.sh"]
     print("Launching docker with args %s" % docker_args, file=sys.stderr)
     return subprocess.call(docker_args)
