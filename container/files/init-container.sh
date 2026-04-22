2f970328312f4de3c29152d3548ac4fe4506701a
Yann Dirson <yann.dirson@vates.tech>
2026-04-22 18:51:51 +0200

    init-container: set GIT_CEILING_DIRECTORIES to avoid corrupting XAPI version
    Unlike originally stated in https://github.com/ocaml/dune/issues/11225
    setting GIT_CEILING_DIRECTORIES actually prevents dune-build-info from
    mis-identifying the SRPM git repository as xen-api.git, and should stop
    corrupting the version in installed binaries.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- container/files/init-container.sh
+++ container/files/init-container.sh
@@ -128,6 +128,7 @@ if [ -n "$BUILD_LOCAL" ]; then
         # in case the build deps contain xs-opam-repo, source the added profile.d file
         [ ! -f /etc/profile.d/opam.sh ] || source /etc/profile.d/opam.sh
         if [ $? == 0 ]; then
+            export GIT_CEILING_DIRECTORIES=$PWD/BUILD
             rpmbuild "${RPMBUILDFLAGS[@]}"
             if [ $? == 0 -a -d ~/output/ ]; then
                 cp -rf RPMS SRPMS ~/output/
